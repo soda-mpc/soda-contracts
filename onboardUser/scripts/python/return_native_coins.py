24361a4ff082ba6095474ab4bdc0d547d87b7c82
SodAsaf <150155735+SodAsaf@users.noreply.github.com>
2024-05-30 14:15:35 +0300

    Add a script to return all native currency back to the funder (#46)

--- onboardUser/scripts/python/return_native_coins.py
+++ onboardUser/scripts/python/return_native_coins.py
@@ -4,25 +4,23 @@ from eth_account import Account
 from lib.python.soda_web3_helper import SodaWeb3Helper, DEFAULT_GAS_PRICE, LOCAL_PROVIDER_URL, REMOTE_HTTP_PROVIDER_URL
 import argparse
 
-def transfer_to_account(sender_account_key, receiver_account, provider_url):
-    soda_helper = SodaWeb3Helper(sender_account_key, provider_url)
+def transfer_all_funds_back_to_account(returning_account_key, receiver_account, provider_url):
+    soda_helper = SodaWeb3Helper(returning_account_key, provider_url)
     sender_balance = soda_helper.get_native_currency_balance(soda_helper.account.address)
-    original_receiver_balance = soda_helper.get_native_currency_balance(receiver_account.address)
+    print(f"Initial balance of the sender account: {soda_helper.wei_to_sod(sender_balance)}")
     if sender_balance is None:
         print("Failed to get the balance of the account")
-    amount_to_transfer = 1  # in SOD, an ether-like unit
-    receipt = soda_helper.transfer_native_currency(receiver_account.address, amount_to_transfer)
+    amount_to_transfer = soda_helper.wei_to_sod(sender_balance - 21000 * DEFAULT_GAS_PRICE)  # in SOD, an ether-like unit
+    soda_helper.transfer_native_currency(receiver_account.address, amount_to_transfer)
     # We check the balance of the accounts after the transfer is complete
-    receiver_balance = soda_helper.get_native_currency_balance(receiver_account.address)
-    assert receiver_balance == original_receiver_balance + soda_helper.convert_sod_to_wei(amount_to_transfer)
-    print(f"Balance of the receiver account: {receiver_balance}")
-    
+    sender_balance = soda_helper.get_native_currency_balance(soda_helper.account.address)
+    print(f"Final balance of the receiver account: {soda_helper.wei_to_sod(sender_balance)}")
 
-def main(sender_account_key, provider_url):
+def main(return_account_key, provider_url):
     signing_key = os.environ.get('SIGNING_KEY')
-    receiver_account = Account.from_key(signing_key)
+    receiver_account = Account.from_key(return_account_key)
     print(f"Receiver account address: {receiver_account.address}")
-    transfer_to_account(sender_account_key, receiver_account, provider_url)
+    transfer_all_funds_back_to_account(signing_key, receiver_account, provider_url)
 
 
 if __name__ == "__main__":
